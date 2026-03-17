136d0dfe708e514f4eb21a9de0da1ae1be50158c
FuseCPDevOPS <DevOPS@fusecp.com>
2026-03-17 23:04:41 +0100

    Load provider assemblies from Providers folder at runtime

--- FuseCP/Sources/FuseCP.Server/Code/Program.cs
+++ FuseCP/Sources/FuseCP.Server/Code/Program.cs
@@ -15,17 +15,72 @@
 
 #if !NETFRAMEWORK
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+
 namespace FuseCP.Server
 {
 	public static class Program
 	{
+		private static bool providerResolverConfigured;
+		private static Dictionary<string, string> providerAssemblyMap;
 
 		public static void Main(string[] args)
 		{
 			//if (!Debugger.IsAttached) Debugger.Launch();
+			ConfigureProviderAssemblyResolver();
 			PasswordValidator.Init();
 			FuseCP.Web.Services.StartupCore.Init(args);
 			FuseCP.Server.Utils.Log.LogLevel = FuseCP.Web.Services.Configuration.TraceLevel;
+		}
+
+		private static void ConfigureProviderAssemblyResolver()
+		{
+			if (providerResolverConfigured)
+				return;
+
+			providerResolverConfigured = true;
+			providerAssemblyMap = BuildProviderAssemblyMap();
+
+			AssemblyLoadContext.Default.Resolving += (context, assemblyName) =>
+			{
+				if (assemblyName == null || string.IsNullOrEmpty(assemblyName.Name))
+					return null;
+
+				if (providerAssemblyMap.TryGetValue(assemblyName.Name, out var assemblyPath) && File.Exists(assemblyPath))
+					return context.LoadFromAssemblyPath(assemblyPath);
+
+				return null;
+			};
+		}
+
+		private static Dictionary<string, string> BuildProviderAssemblyMap()
+		{
+			var map = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+			foreach (var providerRoot in GetProviderProbeRoots())
+			{
+				if (!Directory.Exists(providerRoot))
+					continue;
+
+				foreach (var file in Directory.EnumerateFiles(providerRoot, "*.dll", SearchOption.AllDirectories))
+				{
+					var name = Path.GetFileNameWithoutExtension(file);
+					if (!map.ContainsKey(name))
+						map[name] = file;
+				}
+			}
+
+			return map;
+		}
+
+		private static IEnumerable<string> GetProviderProbeRoots()
+		{
+			var baseDir = AppContext.BaseDirectory;
+			yield return Path.Combine(baseDir, "Providers");
+			yield return Path.GetFullPath(Path.Combine(baseDir, "..", "bin", "Providers"));
 		}
 	}
 }
